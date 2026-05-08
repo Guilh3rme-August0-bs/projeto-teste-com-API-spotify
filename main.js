5ab775cb4ff56f4f37c5f67519dc0f7de76762bc
Guilherme <gsilva12401321@gmail.com>
2026-05-08 11:48:37 -0300

    função de pesquisar já retorna dados, futuramente podem ser tratados e exibidos em um html

--- main.js
+++ main.js
@@ -5,15 +5,32 @@ require('dotenv').config()
 //const { gerarToken } = require("./autenticacao.js")
 
 const PORT = 3000
-const url = 'https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb'
 
-app.get('/get', async (req, res) => {
+//rota para pesquisar artista, album, track
+
+let urlSearch = 'https://api.spotify.com/v1/search?q=${contentPlus}&type=${typeSearch}&limit=${limit}&include_external=audio'
+
+const typeSearch = ['album', 'artist', 'track']
+const index = 2
+const limit = [10, 20, 50, 100]
+const indexLimit = 0
+const content = 'Michael Jackson'
+const contentPlus = content.replace('', '+')
+
+//'https://api.spotify.com/v1/search?q=${contentPlus}&type=${typeSearch}&limit=${limit[indexLimit]}&include_external=audio'
+
+urlSearchReplace = urlSearch.replace('${contentPlus}', contentPlus)
+    .replace('${typeSearch}', typeSearch[index])
+    .replace('${limit}', limit[indexLimit])
+
+app.get('/search', async (req, res) => {
 
     //const token = await gerarToken();
 
     //preencher (no ".env") com token gerado no "autenticacao.js"
     const token = process.env.TOKEN
-    const resposta = await fetch('https://api.spotify.com/v1/browse/new-releases', {
+    const resposta = await fetch(urlSearchReplace, {
+        method: 'GET',
         headers: {
             Authorization: `Bearer ${token}`
         }
@@ -21,10 +38,20 @@ app.get('/get', async (req, res) => {
 
     const data = await resposta.json();
 
-    console.log("STATUS:", resposta.status);
-    console.log(data);
+    const resultado = data.tracks.items.map(track => ({
+        nome: track.name,
+        artista: track.artists[0].name,
+        album: track.album.name,
+        imagem: track.album.images[0]?.url
+       
+    }))
 
-    res.send(data);
+    res.json(resultado)
+
+    console.log("STATUS:", resposta.status);
+    //console.log(data);
+
+    res.send(resultado);
 });
 
 app.listen(PORT, () => {
