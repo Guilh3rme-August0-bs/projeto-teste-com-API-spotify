c456f5fd4424c1cd1b12be90af6f504cdddc32fd
Guilherme <gsilva12401321@gmail.com>
2026-05-12 11:36:58 -0300

    função de buscar tracks foi concluída

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,8 @@ const url = 'http://127.0.0.1:3000/search/?'
 
 const textArea = document.getElementById('textArea')
 
+var list = []
+
 async function search(content, type) {
 
     const contentPlus = content.replace(/ /g, '+')
@@ -9,14 +11,16 @@ async function search(content, type) {
     let resposta = await fetch(`${url}content=${contentPlus}&type=${type}`)
 
     let resultado = await resposta.json()
+
     
+    list = resultado
     textArea.innerText = JSON.stringify(resultado, null, 2)
     console.log(`tamanho do retorno: ${resultado.length}`)
+    listar()
 }
 
 document.querySelector('.search-button').addEventListener('click', function () {
 
-console.log('siiiim')
 
     const content = document.getElementById('search-input')
     const type = document.getElementById('select-input')
@@ -27,3 +31,45 @@ console.log('siiiim')
     search(contentValue, typeValue)
 
 });
+
+//criar lista com resultados
+
+function listar() {
+
+    /* 
+    
+        nome
+        artista
+        album
+        imagem
+        preview
+
+    */
+    
+    for (let i = 0; i < list.length; i++) {
+
+        const body = document.querySelector('.results')
+        const lista_div = document.createElement('div')
+        lista_div.classList.add('item')
+
+        const lista_anterior = document.querySelectorAll('.item')
+
+        if (lista_anterior.length >= 10) {
+            lista_anterior.forEach(el => el.remove())
+        }
+
+        lista_div.innerHTML = `<div class="lista_div">
+            <img class="cover_icon" src=${list[i].imagem}>
+            <div class="item_content">
+                <ul>
+                    <li class="nome">${list[i].nome}</li>
+                    <li class="artista">${list[i].artista}</li>
+                    <li class="album">${list[i].album}</li>
+                </ul>
+            </div>
+        </div>`
+
+        body.appendChild(lista_div)
+        console.log(`item: ${i}`)
+    }
+}
